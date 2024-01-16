d8978531c4e3d0b25082f12be842eed6f649a4ed
gigikale <luckygloria93@gmail.com>
2024-01-16 13:47:03 +0100

    first commit

---
+++ src/main/java/com/example/activitytrackertask/ActivityTrackerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitytrackertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTrackerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityTrackerTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/activitytrackertask/ActivityTrackerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitytrackertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityTrackerTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
